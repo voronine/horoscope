56e87c4bbac739004e3df90e1f5b368221d71901
unknown <voronine@ukr.net>
2025-04-12 14:22:50 +0300

    logo

--- src/components/ZodiacLogo.tsx
+++ src/components/ZodiacLogo.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useSelector } from 'react-redux'
 
 type ZodiacLogoProps = {
   sign: string
@@ -7,12 +8,14 @@ type ZodiacLogoProps = {
 const myLoader = ({ src }: { src: string }): string => src
 
 const ZodiacLogo: React.FC<ZodiacLogoProps> = ({ sign }) => {
+  const theme = useSelector((state: { theme: { theme: 'light' | 'dark' } }) => state.theme.theme)
+  const folder = theme === 'dark' ? 'zodiac-dark' : 'zodiac'
   return (
     <Image
       loader={myLoader}
       unoptimized
       priority
-      src={`/images/zodiac/${sign}.png`}
+      src={`/images/${folder}/${sign}.png`}
       alt={`${sign} logo`}
       width={120}
       height={120}
